3a6ddbab808e7b4fff6975392e726faf620debd7
zuowanghan <91044808+zuowanghan@users.noreply.github.com>
2021-11-21 08:38:40 +0800

    Constants.kt
    Update Constants.kt

---
+++ Constants.kt
@@ -0,0 +1,13 @@
+package com.example.riteshkumarsingh.weatherapplication
+
+/**
+ * Created by riteshkumarsingh on 13/10/17.
+ */
+class Constants {
+    companion object {
+        const val BASE_URL = "http://api.apixu.com"
+        // This api key is not valid, generate your own by logging in here.
+        // https://www.apixu.com/
+        const val key = "f308568837cd42d793d123416170610"
+    }
+}
